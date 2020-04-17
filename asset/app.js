c9a4ce11fefec4820b517a6950003307a498c3c9
kallistahalim <kallistahalim@gmail.com>
2020-04-16 23:45:03 -0700

    stuck

--- asset/app.js
+++ asset/app.js
@@ -12,8 +12,8 @@ var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 function startGame() {
     var numberOfGuessLeft = 9;
     var yourGuess = [];
-    document.querySelector("#win").innerHTML = win;
-    document.querySelector("#lose").innerHTML = lose;
+    // document.querySelector("#win").innerHTML = win;
+    // document.querySelector("#lose").innerHTML = lose;
     document.querySelector("#guess-left").innerHTML = numberOfGuessLeft;
     document.querySelector("#your-guess").innerHTML = yourGuess;
 }
@@ -29,12 +29,12 @@ document.onkeyup = function(event) {
         document.querySelector("#win").innerHTML = win;
         startGame();
     } else {
-        if(numberOfGuessLeft === 0) {
+        numberOfGuessLeft--;
+        if(numberOfGuessLeft < 1) {
             lose ++;
             document.querySelector("#lose").innerHTML = lose;
             startGame();
         } else {
-            numberOfGuessLeft --;
             yourGuess.push(letter);
             document.querySelector("#guess-left").innerHTML = numberOfGuessLeft;
             document.querySelector("#your-guess").innerHTML = yourGuess;
